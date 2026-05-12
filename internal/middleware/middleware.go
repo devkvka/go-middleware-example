7274e8bd57a136b3ca9ca20cb64774d4f6a808b4
DKK <devkvka@gmail.com>
2026-05-12 07:18:57 +0300

    Add documentation

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -5,10 +5,13 @@ import (
 	"slices"
 )
 
+// With applies each middleware function in the order they are passed
 func With(
 	h http.HandlerFunc,
 	middlewares ...func(http.HandlerFunc) http.HandlerFunc,
 ) http.HandlerFunc {
+	// With slices.Backward the middleware gets applied in the order they are
+	// passed in to the function.
 	for _, mw := range slices.Backward(middlewares) {
 		h = mw(h)
 	}
