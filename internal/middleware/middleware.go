41a08f6fdb2b5cc8dd3dce4ee8f15c3256e2892f
DKK <devkvka@gmail.com>
2026-05-12 07:23:54 +0300

    Update middleware.go

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -5,13 +5,13 @@ import (
 	"slices"
 )
 
-// With applies each middleware function in the order they are passed
+// With applies each middleware function in the order they are passed.
+// For example if you pass in Auth then Log, the authorization middleware
+// will be run before any logging happens.
 func With(
 	h http.HandlerFunc,
 	middlewares ...func(http.HandlerFunc) http.HandlerFunc,
 ) http.HandlerFunc {
-	// With slices.Backward the middlewares get applied in the order they are
-	// passed in to the function.
 	for _, mw := range slices.Backward(middlewares) {
 		h = mw(h)
 	}
