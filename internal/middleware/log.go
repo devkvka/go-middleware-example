b3b6309e4decc304e1317d5bd6be45f8b7e9b9d9
DKK <devkvka@gmail.com>
2026-05-12 07:24:15 +0300

    wording

--- internal/middleware/log.go
+++ internal/middleware/log.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// Log is a simple log middleware demonstration
+// Log is a simple logger middleware demonstration
 func Log(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Logger before call -- ", r.RemoteAddr)
